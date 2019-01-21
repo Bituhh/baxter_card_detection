53bfa05737a499490d7c91faefa414cdc1c3e3d5
bituhh <victor@oliveira.org.uk>
2019-01-21 14:42:13 +0000

    Arm class finalised - ToDo: debug for any errors

--- scripts/Arm.py
+++ scripts/Arm.py
@@ -1,8 +1,9 @@
 import baxter_interface
 import rospy
+import struct
 
 from std_msgs.msg import Header
-from baxter_core_msgs.msg import SolvePositionIKRequest
+from baxter_core_msgs.msg import SolvePositionIKRequest, SolvePositionIK
 from geometry_msgs.msg import Pose, PoseStamped
 
 
@@ -10,53 +11,38 @@ class Arm:
     def __init__(self, limb):
         self.limb = baxter_interface.Limb(limb)
         self.pose = Pose()
-        self.set_pose(self.pose)
+        current = self.get_current_pose()
+        self.pose.position.x = current['position'].x
+        self.pose.position.y = current['position'].y
+        self.pose.position.z = current['position'].z
+        self.pose.orientation.x = current['orientation'].x
+        self.pose.orientation.y = current['orientation'].y
+        self.pose.orientation.z = current['orientation'].z
 
     def set_pose(self):
         header = Header(stamp=rospy.Time.now(), frame_id='base')
-        request = SolvePositionIKRequest()
-        request.pose_stamp.append(PoseStamped(header=header, pose=self.ik_pose))
+        ik_request = SolvePositionIKRequest()
+        il_request.pose_stamp.append(PoseStamped(header=header, pose=self.pose))
+        ik_namespace = 'ExternalTools/' + self.limb + '/PositionKinematicsNode/IKService'
+        try:
+            ik_service = rospy.ServiceProxy(ik_namespace, SolvePositionIK)
+            ik_response = ik_service(ik_request)
+            ik_validation = struct.unpack('<%dB' % len(ik_response.result_type), ik_response.result_type)[0]
+            if (ik_validation != ik_response.RESULT_INVALID):
+                joint_position = dict(zip(ik_response.joints[0].name, ik_response.joints[0].position))
+                self.limb.move_to_joint_positions(joint_position)
+            else:
+                rospy.logerr('No Valid Pose Found!')
+        except(rospy.ServiceException, rospy.ROSException), e:
+            rospy.logerr('Service call failed: %s' % (e,))
 
     def get_current_pose(self)
         return self.limb.endpoint.pose()
 
 
-leftPose = {
-        pos_x: 0,
-        pos_y: 0,
-        pos_z: 0,
-        rot_x: 0,
-        rot_y: 0,
-        rot_z: 0}
-leftArm = Arm('left', leftPose)
-
-rightPose = {
-        pos_x: 0,
-        pos_y: 0,
-        pos_z: 0,
-        rot_x: 0,
-        rot_y: 0,
-        rot_z: 0}
-rightArm = Arm('right', rightPose)
-
-leftArm.set_pose()
-
-
-# Loop similar to arduino setup loop!
-def setup():
-    print('Im in setup')
-    pass
-
-def loop():
-    print('Im in loop')
-    #return True 
-
-
-
-
-status = True
-setup()
-while (not loop()):
-    pass
-
+if __name__ == '__main__':
+    leftArm = Arm('left')
+    current = leftArm.get_current_pose()
+    leftArm.pose.position.x = current['position'].x + 10
+    leftArm.set_pose()
 
