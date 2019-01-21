8d1234298d133ebff46c0e06df26dee38cdf0028
bituhh <victor@oliveira.org.uk>
2019-01-21 18:51:48 +0000

    fully working Arm.py

--- scripts/Arm.py
+++ scripts/Arm.py
@@ -1,15 +1,22 @@
+#!/usr/bin/env python
+
 import baxter_interface
+from baxter_interface import CHECK_VERSION
 import rospy
 import struct
 
 from std_msgs.msg import Header
-from baxter_core_msgs.msg import SolvePositionIKRequest, SolvePositionIK
+from baxter_core_msgs.srv import SolvePositionIKRequest, SolvePositionIK
 from geometry_msgs.msg import Pose, PoseStamped
 
 
 class Arm:
-    def __init__(self, limb):
-        self.limb = baxter_interface.Limb(limb)
+    def __init__(self, limb_name):
+        rospy.init_node('rsdk_ik_service_client')
+        baxter_enabler = baxter_interface.RobotEnable(CHECK_VERSION)
+        baxter_enabler.enable()
+        self.limb_name = limb_name
+        self.limb = baxter_interface.Limb(self.limb_name)
         self.pose = Pose()
         current = self.get_current_pose()
         self.pose.position.x = current['position'].x
@@ -22,12 +29,13 @@ class Arm:
     def set_pose(self):
         header = Header(stamp=rospy.Time.now(), frame_id='base')
         ik_request = SolvePositionIKRequest()
-        il_request.pose_stamp.append(PoseStamped(header=header, pose=self.pose))
-        ik_namespace = 'ExternalTools/' + self.limb + '/PositionKinematicsNode/IKService'
+        ik_request.pose_stamp.append(PoseStamped(header=header, pose=self.pose))
+        ik_namespace = 'ExternalTools/' + self.limb_name + '/PositionKinematicsNode/IKService'
         try:
             ik_service = rospy.ServiceProxy(ik_namespace, SolvePositionIK)
             ik_response = ik_service(ik_request)
             ik_validation = struct.unpack('<%dB' % len(ik_response.result_type), ik_response.result_type)[0]
+            print(ik_validation)
             if (ik_validation != ik_response.RESULT_INVALID):
                 joint_position = dict(zip(ik_response.joints[0].name, ik_response.joints[0].position))
                 self.limb.move_to_joint_positions(joint_position)
@@ -36,13 +44,15 @@ class Arm:
         except(rospy.ServiceException, rospy.ROSException), e:
             rospy.logerr('Service call failed: %s' % (e,))
 
-    def get_current_pose(self)
-        return self.limb.endpoint.pose()
+    def get_current_pose(self):
+        return self.limb.endpoint_pose()
 
 
 if __name__ == '__main__':
     leftArm = Arm('left')
     current = leftArm.get_current_pose()
-    leftArm.pose.position.x = current['position'].x + 10
+    print(leftArm.pose.position.x)
+    leftArm.pose.position.x = 0.5
+    print(leftArm.pose.position.x)
     leftArm.set_pose()
 
